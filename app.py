e4f770b1e482bd801b350436f2262ff1bbb7db06
Bigstifff <okewoyeafolabiisaac@gmail.com>
2025-10-16 19:26:00 +0100

    almost done with app set up

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, session, redirect, request, jsonify, render_template
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    render_template("home.html")
