a500173d59736b2cf920bb13273f4b7c279d60f3
Grindpa <francisanyanwuchu@gmail.com>
2025-10-21 12:13:02 +0100

    done with home page UI

--- static/js.js
+++ static/js.js
@@ -2,4 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.body.id === "layout") {
         
     }
+    
+    // js for the home page
+    else if (document.body.id === "home") {
+        let suggest = document.querySelectorAll(".suggest div");
+        let input = document.querySelector("#querry input");
+        let submit = document.querySelector("#querry button");
+
+        submit.disabled = true;
+
+        for (let i = 0; i < suggest.length; i++) {
+            suggest[i].addEventListener("click", (e) => {
+                let x = e.target;
+
+                input.value = x.textContent;
+            })
+        }
+    }
 })
